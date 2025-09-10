ffeaabc11cf6b4806af621b79e4a9c1a36317fad
Nikita Morkovkin <nikitamorkovkinwork@gmail.com>
2025-09-10 19:00:29 +0500

    App routes were teleported in a separate config file.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,20 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import {classNames} from "shared/lib/classNames/classNames";
 import useTheme from "./providers/ThemeProvider/lib/useTheme";
 import './styles/index.scss';
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <h1>React + TS + Webpack</h1>
+      <Link to={'/about'}>
+        About
+      </Link>
 
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О нас</Link>
-
-      <button
-        type="button"
-        className={'button-switch'}
-        onClick={toggleTheme}
-      >
-        {theme === 'light' ? 'Switch dark' : 'Switch light'}
-      </button>
-
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route
-            path="/about"
-            element={<AboutPage />}
-          />
-
-          <Route
-            path="/"
-            element={<MainPage />}
-          />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
